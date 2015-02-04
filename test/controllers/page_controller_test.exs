2bdad05bc4f0231c6f39e152a5fd63fb5a5b3bab
sparker <scott.p.parker@gmail.com>
2015-02-04 17:24:36 -0600

    Simplify my controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -2,22 +2,11 @@ defmodule EdgeBuilder.Controllers.PageControllerTest do
   use ExSpec
   use Plug.Test
 
-  @opts EdgeBuilder.Router.init []
-
-  def with_session(conn) do
-    session_opts = Plug.Session.init(store: :cookie, key: "_app",
-    encryption_salt: "abc", signing_salt: "abc")
-    conn
-    |> Map.put(:secret_key_base, String.duplicate("abcdefgh", 8))
-    |> Plug.Session.call(session_opts)
-    |> Plug.Conn.fetch_session()
-    |> Plug.Conn.fetch_params()
-  end
+  @opts []
 
   describe "reference" do
     it "renders the reference HTML" do
-      conn = conn(:get, "/reference") |> with_session
-      conn = EdgeBuilder.Router.call(conn, @opts)
+      conn = conn(:get, "/reference") |> EdgeBuilder.Endpoint.call(@opts)
 
       assert String.contains?(conn.resp_body, "Character Name")
     end
