c0d2b8d6a2900f3d78788deb6d051b57d815934d
sparker <scott.p.parker@gmail.com>
2015-02-24 21:50:14 -0800

    Adding attacks works on the character form (still have to make removing work)

--- priv/static/js/character_form.js
+++ priv/static/js/character_form.js
@@ -38,10 +38,38 @@ var CharacterForm = (function() {
     refreshSkillByName(skillName);
   }
 
+  function addAttack() {
+    var firstAttackRow = $(".attack-first-row:last").clone(true);
+    var secondAttackRow = $(".attack-second-row:last").clone(true);
+    var previousIndex = parseInt(firstAttackRow.attr("data-attack"));
+    var index = previousIndex + 1;
+    var attackTable = $("#attackTable");
+
+    attackTable.append(firstAttackRow);
+    attackTable.append(secondAttackRow);
+
+    firstAttackRow.find("[type=hidden]").remove();
+    firstAttackRow.find("[type=text]").val("");
+    firstAttackRow.attr("data-attack", index);
+    firstAttackRow.find("[data-attack-roll-index]").attr("data-attack-roll-index", index);
+    firstAttackRow.find("[data-attack-index]").attr("data-attack-index", index);
+    firstAttackRow.find("select").val(function() { return $(this).find("option:first").val() }).change();
+    firstAttackRow.toggleClass("active");
+    firstAttackRow.find("[name]").attr("name", function(i, currentName) { return currentName.replace("attacks["+previousIndex+"]", "attacks["+index+"]") });
+    firstAttackRow.find("[for]").attr("for", function(i, currentFor) { return currentFor.replace("attacks["+previousIndex+"]", "attacks["+index+"]") });
+
+    secondAttackRow.find("[type=text]").val("");
+    secondAttackRow.attr("data-attack", index);
+    secondAttackRow.toggleClass("active");
+    secondAttackRow.find("[name]").attr("name", function(i, currentName) { return currentName.replace("attacks["+previousIndex+"]", "attacks["+index+"]") });
+    secondAttackRow.find("[for]").attr("for", function(i, currentFor) { return currentFor.replace("attacks["+previousIndex+"]", "attacks["+index+"]") });
+  }
+
   function initializeHandlers() {
     $("[data-skill-value]").change(function() { refreshSkillByName($(this).attr("data-skill-value")) });
     $("[data-characteristic]").change(function() { refreshSkillsFromCharacteristic($(this)) });
     $("[data-attack-skill]").change(function() { switchAttackRollFromSelection($(this)) });
+    $("#addAttackButton").click(addAttack);
   }
 
   function refreshAllDice() {
