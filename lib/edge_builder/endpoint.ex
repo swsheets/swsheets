c51539ea7ca4c6b9f7e4c1477c7b59b432d7d8b8
sparker <scott.p.parker@gmail.com>
2015-06-02 17:51:57 -0500

    Add a ton of favicons

--- lib/edge_builder/endpoint.ex
+++ lib/edge_builder/endpoint.ex
@@ -3,7 +3,37 @@ defmodule EdgeBuilder.Endpoint do
 
   plug Plug.Static,
     at: "/", from: :edge_builder,
-    only: ~w(css images js fonts)
+    only: ~w(css images js fonts
+      favicon-16x16.png
+      android-chrome-72x72.png
+      mstile-150x150.png
+      mstile-310x150.png
+      mstile-310x310.png
+      android-chrome-96x96.png
+      android-chrome-192x192.png
+      apple-touch-icon-152x152.png
+      apple-touch-icon-76x76.png
+      android-chrome-48x48.png
+      favicon.ico
+      favicon-96x96.png
+      favicon-32x32.png
+      apple-touch-icon-180x180.png
+      apple-touch-icon-60x60.png
+      apple-touch-icon-72x72.png
+      apple-touch-icon.png
+      apple-touch-icon-120x120.png
+      manifest.json
+      apple-touch-icon-114x114.png
+      apple-touch-icon-precomposed.png
+      mstile-144x144.png
+      browserconfig.xml
+      apple-touch-icon-144x144.png
+      android-chrome-144x144.png
+      favicon-194x194.png
+      android-chrome-36x36.png
+      apple-touch-icon-57x57.png
+      mstile-70x70.png
+    )
 
   plug Plug.Logger
 
