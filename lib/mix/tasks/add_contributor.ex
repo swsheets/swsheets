3a2732fcf28abf209416507fd4f0b59fb48128e0
sparker <scott.p.parker@gmail.com>
2015-06-04 13:09:59 -0500

    Add a task to grant contribution status via commandline. Closes #21

---
+++ lib/mix/tasks/add_contributor.ex
@@ -0,0 +1,23 @@
+defmodule Mix.Tasks.AddContributor do
+  use Mix.Task
+
+  alias EdgeBuilder.Models.User
+
+  @shortdoc "Add a contributor to the app. Arguments: USERNAME <pull_request|bug>"
+
+  @moduledoc @shortdoc
+  def run(args)
+  def run([username, "pull_request"]) do
+    set_contribution(username, %{pull_requested_at: Ecto.DateTime.local})
+  end
+  def run([username, "bug"]) do
+    set_contribution(username, %{bug_reported_at: Ecto.DateTime.local})
+  end
+
+  defp set_contribution(username, changes) do
+    EdgeBuilder.Repo.start_link
+    User.by_username(username)
+    |> User.changeset(:contributions, changes)
+    |> EdgeBuilder.Repo.update
+  end
+end
---
+++ test/lib/add_contributor_task_test.exs
@@ -0,0 +1,26 @@
+defmodule EdgeBuilder.Lib.AddContributorTaskTest do
+  use EdgeBuilder.Test
+
+  alias Factories.UserFactory
+  alias EdgeBuilder.Models.User
+
+  describe "run" do
+    it "marks a bug reporter" do
+      user = UserFactory.default_user
+
+      Mix.Tasks.AddContributor.run([user.username, "bug"])
+
+      user = EdgeBuilder.Repo.get(User, user.id)
+      assert !is_nil(user.bug_reported_at)
+    end
+
+    it "marks a pull requester" do
+      user = UserFactory.default_user
+
+      Mix.Tasks.AddContributor.run([user.username, "pull_request"])
+
+      user = EdgeBuilder.Repo.get(User, user.id)
+      assert !is_nil(user.pull_requested_at)
+    end
+  end
+end
