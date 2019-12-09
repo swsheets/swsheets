e3641d7069d2b009109fd8d603ed85bbb5ac3925
Nicholas Duffy <duffn@outlook.com>
2019-12-09 12:10:08 -0700

    Fix quoted keyword warning

--- mix.exs
+++ mix.exs
@@ -49,6 +49,6 @@ defmodule EdgeBuilder.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     test: ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
