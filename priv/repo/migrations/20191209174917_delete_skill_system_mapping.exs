7043520b3ef726be7b6e79f861905fc54d36c080
sparker <scott.p.parker@gmail.com>
2019-12-14 13:57:23 -0600

    Postpone deleting the system column until we have a successful deployment

--- priv/repo/migrations/20191209174917_delete_skill_system_mapping.exs
+++
@@ -1,9 +0,0 @@
-defmodule EdgeBuilder.Repo.Migrations.DeleteSkillSystemMapping do
-  use Ecto.Migration
-
-  def change do
-    alter table(:base_skills) do
-      remove(:system)
-    end
-  end
-end
