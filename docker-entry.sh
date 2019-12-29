c192b20ae2c6f97c13fb4abb25c68cf32146184a
Nicholas Duffy <duffn@outlook.com>
2019-12-29 10:51:34 -0700

    Update docker-entry file

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
+echo "Installing dependencies..."
+mix deps.get
+
 until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
   >&2 echo "Postgres is unavailable: sleeping..."
   sleep 1
 done
 
 if [ "${1}" = "setup" ]; then
-  echo "Creating database user and extensions..."
+  echo "Creating database user..."
   createuser -h db -U postgres pair -d
-  psql -h db -U postgres edgebuilder_development -c "CREATE EXTENSION pgcrypto"
-
+  
   echo "Creating and migrating development database..."
   mix do ecto.drop, ecto.create
+  psql -h db -U postgres edgebuilder_development -c "CREATE EXTENSION pgcrypto"
   mix do ecto.migrate, seed
 else
-  echo "Installing dependencies..."
-  mix deps.get
-
-  echo "Launching Phoenix web server..."
+  echo "Starting Phoenix server..."
   mix phx.server
 fi
