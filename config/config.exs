0f6273708b293ccb2bb2e8fba78394f7215f07e1
Nicholas Duffy <duffn@outlook.com>
2020-01-04 19:03:51 -0700

    Add Docker setup for local development (#134)
    * Initial commit of local Docker development
    
    * Update config file for Docker
    
    * Update config.exs
    
    * Update docker-entry file
    
    * Revert mix.exs and mix.lock changes
    
    * Trigger CircleCI
    
    * Fix merge confilict
    
    * Update README and entrypoint for Docker tests
    
    * Remove mix helper in favor of straight docker-compose

--- config/config.exs
+++ config/config.exs
@@ -27,9 +27,11 @@ config :logger, :console,
 
 config :edge_builder, EdgeBuilder.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: "edgebuilder_development",
-  username: "pair",
-  hostname: "localhost",
+  username: System.get_env("DB_USER") || "pair",
+  password: System.get_env("DB_PASSWORD") || "",
+  database: System.get_env("DB_NAME") || "edgebuilder_development",
+  hostname: System.get_env("DB_HOST") || "localhost",
+  port: System.get_env("DB_PORT") || 5432,
   pool_size: 10
 
 config :edge_builder,
---
+++ docker-entry.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+echo "Installing dependencies..."
+mix deps.get
+
+until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable: sleeping..."
+  sleep 1
+done
+
+if [ "${1}" = "setup" ]; then
+  echo "Creating database user..."
+  createuser -h db -U postgres pair -d
+  
+  echo "Creating and migrating development database..."
+  mix do ecto.drop, ecto.create
+  psql -h db -U postgres edgebuilder_development -c "CREATE EXTENSION pgcrypto"
+  mix do ecto.migrate, seed
+elif [ "${1}" = "test" ]; then
+  echo "Running tests..."
+  MIX_ENV=test mix do ecto.drop, ecto.create
+  psql -h db -U postgres edgebuilder_test -c "CREATE EXTENSION pgcrypto"
+  MIX_ENV=test mix do ecto.migrate, seed
+  mix test
+elif [ ! -z ${1} ]; then
+  mix "$@"
+else
+  echo "Starting Phoenix server..."
+  mix phx.server
+fi
