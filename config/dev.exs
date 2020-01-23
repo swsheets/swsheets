48c8068780e20593f6252e0c90753f54b4ab7ea6
Nicholas Duffy <duffn@outlook.com>
2020-01-22 21:14:09 -0700

    Make code reloading work properly

--- config/dev.exs
+++ config/dev.exs
@@ -26,12 +26,19 @@ config :edge_builder, EdgeBuilder.Endpoint,
 config :edge_builder, EdgeBuilder.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
-      ~r{priv/gettext/.*(po)$},
-      ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"priv/gettext/.*(po)$",
+      ~r"web/views/.*(ex)$",
+      ~r"web/templates/.*(eex)$"
     ]
   ]
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
+
+# Initialize plugs at runtime for faster development compilation
+config :phoenix, :plug_init_mode, :runtime
