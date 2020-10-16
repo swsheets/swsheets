730875b6837f3428c80399b255f644fe92d795cf
Nicholas Duffy <duffn@outlook.com>
2020-10-16 10:42:42 -0600

    Duffn/postgres local password (#274)

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+until PGPASSWORD=password1 psql -h db -U "postgres" -c '\q' 2>/dev/null; do
   >&2 echo "Postgres is unavailable: sleeping..."
   sleep 1
 done
 
 if [ "${1}" = "setup" ]; then
   echo "Creating database user..."
-  createuser -h db -U postgres pair -d
-  
+  PGPASSWORD=password1 psql -h db -U postgres -c "CREATE USER pair with PASSWORD 'password1' CREATEDB;"
+
   echo "Creating and migrating development database..."
   mix do ecto.drop, ecto.create
-  psql -h db -U postgres edgebuilder_development -c "CREATE EXTENSION pgcrypto"
+  PGPASSWORD=password1 psql -h db -U postgres edgebuilder_development -c "CREATE EXTENSION pgcrypto"
   mix do ecto.migrate, seed
 elif [ "${1}" = "test" ]; then
   echo "Running tests..."
   MIX_ENV=test mix do ecto.drop, ecto.create
-  psql -h db -U postgres edgebuilder_test -c "CREATE EXTENSION pgcrypto"
+  PGPASSWORD=password1 psql -h db -U postgres edgebuilder_test -c "CREATE EXTENSION pgcrypto"
   MIX_ENV=test mix do ecto.migrate, seed
   mix test "${@:2}"
 elif [ ! -z ${1} ]; then
