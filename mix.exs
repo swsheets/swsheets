49d5a90904caa5cf719c14ee9b4bc3b50bf50faa
Nicholas Duffy <duffn@outlook.com>
2021-07-10 13:23:54 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EdgeBuilder.Mixfile do
   def project do
     [
       app: :edge_builder,
-      version: "2.15.1",
+      version: "2.16.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
