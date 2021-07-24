600e709e62aa947a1641e230a5bd8f34eea7c986
Nicholas Duffy <duffn@outlook.com>
2021-07-24 09:48:35 -0600

    Update webpack loader syntax

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -24,9 +24,7 @@ module.exports = (env, options) => ({
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ["babel-loader"],
       },
       {
         test: /\.css$/,
@@ -34,7 +32,7 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: "url-loader?limit=100000",
+        use: ["url-loader?limit=100000"],
       },
     ],
   },
